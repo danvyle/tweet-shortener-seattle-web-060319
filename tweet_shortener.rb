287ef79fbe569e73f9730eb645eba6bea9745927
Dan-Vy Le <danvyle85@gmail.com>
2019-05-23 23:58:13 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -21,7 +21,7 @@ end
 
 def word_substituter(tweet)
   array = tweet.split.map do |word|
-    if array.keys.include?(word.downcase)
+    if dictionary.keys.include?(word.downcase)
       word = dictionary[word]
     end
   end
