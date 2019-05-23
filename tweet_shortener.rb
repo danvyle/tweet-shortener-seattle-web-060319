df958bc32b3fe55c71975f212d0a600bcda69946
Dan-Vy Le <danvyle85@gmail.com>
2019-05-23 23:12:10 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,21 @@
 # Write your code here.
 
-def dictionary
-  
+def dictionary(key, value)
+dictionary = {
+  "hello": "hi",
+  "to": "2",
+  "two": "2",
+  "too": "2",
+  "for": "4",
+  "four": "4",
+  "be" : "b",
+  "you": "u",
+  "at": "@",
+  "and": "&"}
+
+
+myString.gsub("PHP", "Ruby")
+=> "Welcome to Ruby Essentials!"
 end
 
 def word_subsituter(tweet)
