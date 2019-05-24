ff3c0936c5f022389f29a84062fd6c911543074a
Dan-Vy Le <danvyle85@gmail.com>
2019-05-24 00:13:49 +0000

    Automatically backed up by Learn

---
+++ tweet_shortener.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+ def dictionary
+    dictionary = {
+        "hello" => "hi",
+        "to" => "2",
+        "two" => "2",
+        "too" => "2",
+        "for" => "4",
+        "four" => "4",
+        "you" => "u",
+        "at" => "@",
+        "and" => "&"
+    }
+end
+
+ def word_substituter(tweet)
+    tweet_array = tweet.split.map do |word|
+        if dictionary.keys.include?(word.downcase)
+                word = dictionary[word.downcase]
+        else
+            word
+        end
+    end
+    puts tweet_array.join(" ")
+end
